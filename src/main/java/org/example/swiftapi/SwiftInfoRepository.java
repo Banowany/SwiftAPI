86359386c6a3c95b49837fc46712b3dbd126b16f
Krzysztof <krzysztof.gebka2001@gmail.com>
2025-05-02 18:51:10 +0200

    Implement repositry for SwiftInfo

---
+++ src/main/java/org/example/swiftapi/SwiftInfoRepository.java
@@ -0,0 +1,12 @@
+package org.example.swiftapi;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SwiftInfoRepository extends JpaRepository<SwiftInfo, String> {
+    List<SwiftInfo> findBySwiftCode8(String swiftCode8);
+    List<SwiftInfo> findByCountryISO2(String countryISO2);
+}
