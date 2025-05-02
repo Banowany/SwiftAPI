a6a91ff13ad79fe77e266fd90850f7befdf63ac7
Krzysztof <krzysztof.gebka2001@gmail.com>
2025-05-02 18:34:43 +0200

    remove isHeadquarter field from SwiftInfo model

--- src/main/java/org/example/swiftapi/SwiftInfo.java
+++ src/main/java/org/example/swiftapi/SwiftInfo.java
@@ -18,6 +18,5 @@ public class SwiftInfo {
     private String bankName;
     private String countryISO2;
     private String countryName;
-    private boolean isHeadquarter;
     private String swiftCode8;
 }
